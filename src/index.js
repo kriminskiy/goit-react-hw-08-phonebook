d6b07221df16afa89498c13bda4a879715dd3776
kriminskiy <93137873+kriminskiy@users.noreply.github.com>
2022-09-10 17:54:14 +0300

    hw2

--- src/index.js
+++ src/index.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-
 import App from './App';
 import { store, persistor } from './redux/store';
-
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
-
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
           <App />
         </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
 );
